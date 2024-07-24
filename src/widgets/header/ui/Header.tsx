0fd60abb803c728de5222b36a8f764b46c3fdf44
gnslgns0 <gnslgns0@gmail.com>
2024-07-24 10:22:19 +0900

    폰트 전역설정 및 로딩스크린 작업중

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 h-24 bg-[#F2F2F2] z-10">
       <div className="pt-11 px-4 flex justify-between items-center">
-        <h1 className="font-['Sandoll_Nemony2'] italic font-normal text-xl text-black">세모클래스</h1>
+        <h1 className="font-sm font-normal text-xl text-black">세모클래스</h1>
         <Icon
           name="notification"
           className="w-8 h-8"
---
+++ src/widgets/loading/ui/LoadingScreen.tsx
@@ -0,0 +1,3 @@
+export function LoadingScreen() {
+  return <div className="w-full h-screen flex justify-center max-w-[640px] bg-white my-0 mx-auto">loading...</div>;
+}
