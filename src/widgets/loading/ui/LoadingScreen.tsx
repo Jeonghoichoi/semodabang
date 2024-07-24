5fe6276c7403da466dca1743361ea0b3ef6eccb4
gnslgns0 <gnslgns0@gmail.com>
2024-07-24 13:15:27 +0900

    mainContents 컴포넌트 에러 주석 반영

--- src/widgets/loading/ui/LoadingScreen.tsx
+++ src/widgets/loading/ui/LoadingScreen.tsx
@@ -1,3 +1,17 @@
 export function LoadingScreen() {
-  return <div className="w-full h-screen flex justify-center max-w-[640px] bg-white my-0 mx-auto">loading...</div>;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50">
+      <div className="flex items-center justify-center h-full">
+        <span className="animate-bounce bg-white h-2 w-2 rounded-full" />
+        <span
+          className="animate-bounce bg-white h-2 w-2 rounded-full ml-2"
+          style={{ animationDelay: '0.2s' }}
+        />
+        <span
+          className="animate-bounce bg-white h-2 w-2 rounded-full ml-2"
+          style={{ animationDelay: '0.4s' }}
+        />
+      </div>
+    </div>
+  );
 }
