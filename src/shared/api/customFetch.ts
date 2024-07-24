5068a06202bb9bd0a5ccbc51bf62f3c9a3507730
jslee <jslee@kyobobook.co.kr>
2024-07-24 09:50:27 +0900

    concurrently 추가로 mock 서버와 next 동시 실행 & global fetcher 추가

---
+++ src/shared/api/customFetch.ts
@@ -0,0 +1,22 @@
+import returnFetch from 'return-fetch';
+
+export const customFetch = returnFetch({
+  baseUrl: 'http://localhost:9090',
+  headers: { Accept: 'application/json' },
+  interceptors: {
+    request: async args => {
+      console.log('********* before sending request *********');
+      console.log('url:', args[0].toString());
+      console.log('requestInit:', args[1], '\n\n');
+      return args;
+    },
+
+    response: async (response, requestArgs) => {
+      console.log('********* after receiving response *********');
+      console.log('url:', requestArgs[0].toString());
+      console.log('requestInit:', requestArgs[1], '\n\n');
+      console.log(response);
+      return response;
+    },
+  },
+});
